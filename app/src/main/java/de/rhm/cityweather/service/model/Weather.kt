d423cb1aeebbf2ac654d74d38c68842ebbe8f5a0
ralphhm <ralphhm+github@gmail.com>
2018-06-19 03:03:55 +0200

    Make release build type runnable by adding Proguard exceptions for obfuscation

--- app/src/main/java/de/rhm/cityweather/service/model/Weather.kt
+++ app/src/main/java/de/rhm/cityweather/service/model/Weather.kt
@@ -5,5 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Weather(@Json(name = "name") val name: String, @Json(name = "main") val details: Main) {
+    @JsonClass(generateAdapter = true)
     data class Main(@Json(name = "temp") val temp: String)
 }
